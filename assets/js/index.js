f76588e08eee34a96df1e9d663df3ef53c367559
Shio703 <shiodjamurashvili26@gmail.com>
2025-04-07 18:58:07 +0400

    separated getDate() && getHours() into separated function to ensure, that time will be displayed instantly without any delay at page load

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,15 @@
 const hourCont = document.querySelector(".hourCont");
 const minuteCont = document.querySelector(".minuteCont");
 
-setInterval(() => {
+const clockStarter = () => {
   const hours = new Date().getHours();
   const minutes = new Date().getMinutes();
   hourCont.innerHTML = `${hours.toString().padStart(2, "0")}`;
   minuteCont.innerHTML = `${minutes.toString().padStart(2, "0")}`;
+};
+clockStarter();
+
+setInterval(() => {
+  clockStarter();
   console.log("tick");
 }, 1000);
